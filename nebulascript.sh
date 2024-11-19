c80e5147edb8965c28d4f562bb92d42e5947e5c1
Kartvya69 <148194098+Kartvya69@users.noreply.github.com>
2024-11-19 13:56:19 +0530

    Update nebulascript.sh
    Fixed Nodejs Installation script

--- nebulascript.sh
+++ nebulascript.sh
@@ -18,7 +18,9 @@ install_nodejs() {
     fi
     
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-    source ~/.bashrc  # Ensure nvm is available
+    export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion  # Ensure nvm is available
     
     nvm install 22
     node -v
